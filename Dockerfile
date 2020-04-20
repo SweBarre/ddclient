98dece6f2847fdf79643d0bdb633bfe441b61a15
Jonas Forsberg <jonas.forsberg@suse.com>
2020-04-20 16:35:51 +0200

    fixed working version and ssl

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,15 @@
 FROM opensuse/tumbleweed
 
-RUN zypper install --no-confirm \
+RUN zypper install --no-confirm --no-recommends \
          openSUSE-release-appliance-docker \
          ddclient \
+         perl-IO-Socket-SSL \
     && zypper clean --all \
+    && usermod -s /bin/bash ddclient \
     && mkdir -m 0750 /srv/ddclient \
     && chown ddclient /srv/ddclient
 
 COPY entrypoint.sh /usr/local/bin
 
 ENTRYPOINT ["/usr/local/bin/entrypoint.sh"]
-CMD ["ddclient", "-foreground", "-cache", "/srv/ddclient/ddclient.cache", "-file /srv/ddclient/ddclient.conf"]
+CMD ["/usr/sbin/ddclient", "-foreground", "-cache", "/srv/ddclient/ddclient.cache", "-file /srv/ddclient/ddclient.conf"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,18 +7,19 @@ RGID="${RGID:-$(id -g ddclient)}"
 [[ "$(id -u ddclient)" -ne "$RUID" ]] && usermod -u "$RUID" -o ddclient
 [[ "$(id -g ddclient)" -ne "$RGID" ]] && groupmod -g "$RGID" -o ddclient
 
-if [[ ! -f /etc/ddclient/config ]]; then
+if [[ ! -f /srv/ddclient/ddclient.conf ]]; then
     cp /etc/ddclient.conf /srv/ddclient/ddclient.conf
 fi
-chmown ddclient /srv/ddclient/ddclient.conf
+chown ddclient /srv/ddclient/ddclient.conf
+chmod 0600 /srv/ddclient/ddclient.conf
 
 if [[ "$1" == "reset-permission" ]]; then
   printf "chown -R ddclient:ddclient /srv/ddclient\n"
   chown -R ddclient:ddclient /srv/ddclient
-elif [[ "$1" == "ddclient" ]]; then
+elif [[ "$1" == "/usr/sbin/ddclient" ]]; then
   printf "$*\n"
   printf "Starting ddclient\n"
-  exec su - ddclient "$*"
+  exec su - ddclient -c "$*"
 else
   exec "$@"
 fi
